0007fbcb592dbe5fced7518c3243331b05ea4279
nxsbertram <bertram@nexus-netsoft.com>
2018-03-20 13:57:25 +0100

    * AWS fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 if [ ! -f ~/.aws/credentials ]; then
-    mkdir -p ~/.aws
-    touch credentials
-    echo "[default]" > ~/.aws/credentials
-    echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
-    echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+    if [[ -z "${$AWS_ACCESS_KEY_ID}" ]]; then
+        ACCESS_ID=${AWS_ACCESS_KEY_ID}
+        ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+    else
+        ACCESS_ID=""
+        ACCESS_KEY=""
+    fi
+
+    if [[ "$ACCESS_ID" != "" ]]; then
+        mkdir -p ~/.aws
+        touch credentials
+        echo "[default]" > ~/.aws/credentials
+        echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+        echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+    fi
 fi
 
 /usr/local/bin/$@
